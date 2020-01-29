79d0abe0e6032b88fdeb78c7a38fea18a1dbfb78
peterson <petersantos000@gmail.com>
2020-01-28 22:44:43 -0500

    Added lesson 11 - Values versus reference Types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,21 +8,30 @@ namespace HelloWorld
 {
     class Program
     {
+        static void MultipleLegs(PetStruct petStruct, PetClass petClass)
+        {
+            petStruct.Legs = petStruct.Legs * 2;
+            petClass.Legs = petClass.Legs * 2;
+
+            Console.WriteLine("Internal Method - A " + petStruct.Type + " has " + petStruct.Legs + " Legs.");
+            Console.WriteLine("Internal Method - A " + petClass.Type + " has " + petClass.Legs + " Legs.");
+        }
         static void Main(string[] args)
         {
-
-            PetStruct dog;
-            //dog.Type = "Dog"; Dog is no lnger string after add enum
+            PetStruct dog = new PetStruct();
             dog.Type = PetType.Dog;
             dog.HasFur = true;
             dog.Legs = 4;
 
             PetClass duck = new PetClass();
-            //duck.Type = "Duck";
             duck.Type = PetType.Duck;
             duck.HasFur = false;
             duck.Legs = 2;
 
+            Console.WriteLine("A " + dog.Type + " has " + dog.Legs + " legs.");
+            Console.WriteLine("A " + duck.Type + " has " + duck.Legs + " legs.");
+
+            MultipleLegs(dog, duck);
             Console.WriteLine("A " + dog.Type + " has " + dog.Legs + " legs.");
             Console.WriteLine("A " + duck.Type + " has " + duck.Legs + " legs.");
 
